174d3c790fe41bd79b009066d5e5a518fca4c858
christianpersichetti <123416098+christianpersichetti@users.noreply.github.com>
2023-01-30 21:04:33 -0600

    1/30 Assignment

--- customer_country.py
+++ customer_country.py
@@ -4,17 +4,19 @@ import csv
 customers = open("customers.csv",'r') 
 customer_country = open('customer_country.csv', 'w')
 
-
+#Reading Through File
 csvreader = csv.reader(customers, delimiter=(','))
 
+#Skip Header
 next(csvreader)
 
+#Write File
 customer_country.write('First Name'+ ' '+ 'Last Name'+' '+ 'Country'+ '\n')
 print()
 for row in csvreader:
     customer_country.write(row[1]+'  '+ row[2]+ '  '+ row[4] + '\n')
 
 
-
+#Close Files
 customers.close()
 customer_country.close()
--- employee_bonus.py
+++ employee_bonus.py
@@ -1,15 +1,17 @@
 import csv
-
+#Open File
 employee_bonus = open('EmployeePay.csv','r')
 
+#Read Through File
 csvreader = csv.reader(employee_bonus, delimiter=(','))
 
+#Header
 print('ID '+ 'First Name '+ 'Last Name '+ 'Salary '+ 'Bonus')    
 next(csvreader)
 
+#Printing File
 for row in csvreader:
     print(row[0],' ', row[1], ' ', row[2], ' ', row[3], ' ', row[4])
 
-
-
+#Close File
 employee_bonus.close()
